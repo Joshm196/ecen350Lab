8041d7ab59aa2ebe68d34b79d88e977511e27d04
Joshm196 <Joshua.Morquecho@gmail.com>
2025-02-17 02:21:18 -0600

    Lab 3

--- Lab03/main.c
+++ Lab03/main.c
@@ -4,9 +4,23 @@
 
 extern long long int test(long long int a, long long int b);
 
+extern long long int lab03b();
+
+extern long long int my_array[10];
+extern long long int lab03c();
+
 int main(void)
 {
-    long long int a = test(3, 5);
-    printf("Result of test(3, 5) = %lld\n", a);
+    //~ long long int a = test(3, 5);
+    //~ printf("Result of test(3, 5) = %lld\n", a);
+    
+    //printf("Result of lab03b() = %lld\n", lab03b());
+    lab03c();
+    printf("Result of lab03c():\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%lld ", my_array[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
