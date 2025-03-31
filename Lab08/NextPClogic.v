5e948ea0c0d0842b810528325dbfe254eab0a390
Sanjit Mukesh <105826663+sanjitmukesh@users.noreply.github.com>
2025-03-30 23:28:46 -0500

    Add files via upload

--- Lab08/NextPClogic.v
+++ Lab08/NextPClogic.v
@@ -1,9 +1,12 @@
-module NextPClogic(NextPC, CurrentPC, SignExtImm64, Branch, ALUZero, Uncondbranch); 
-   input [63:0] CurrentPC, SignExtImm64; 
-   input 	Branch, ALUZero, Uncondbranch; 
-   output [63:0] NextPC; 
+`timescale 1ns / 1ps
 
-   /* write your code here */ 
-
-
+module NextPClogic(NextPC, CurrentPC, SignExtImm64, Branch, ALUZero, Uncondbranch);
+    input [63:0] CurrentPC, SignExtImm64; 
+    input Branch, ALUZero, Uncondbranch; 
+    output [63:0] NextPC; 
+    
+    assign NextPC = (Uncondbranch | (Branch && ALUZero)) ? // if B or CBZ
+                    (CurrentPC + (SignExtImm64 << 2)) : // convert instruction offset to byte offset, then add to current pc address
+                    (CurrentPC + 4); // otherwise, go to next instruction
+    
 endmodule
