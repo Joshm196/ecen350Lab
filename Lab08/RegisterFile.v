a429256686bee67e849849c0f6390623dbc3533a
Sanjit Mukesh <105826663+sanjitmukesh@users.noreply.github.com>
2025-03-30 23:46:05 -0500

    Update RegisterFile.v

--- Lab08/RegisterFile.v
+++ Lab08/RegisterFile.v
@@ -1,19 +1,19 @@
 `timescale 1ns / 1ps
 
 module RegisterFile(BusA, BusB, BusW, RA, RB, RW, RegWr, Clk);
-    output [31:0] BusA; //Outputs of data read
-    output [31:0] BusB; 
-    input [31:0] BusW; //Writing data
-    input [4:0] RW, RA, RB; //Read address A, B and Write
+    output [63:0] BusA; // outputs of data read
+    output [63:0] BusB; 
+    input [63:0] BusW; // writing data
+    input [4:0] RW, RA, RB; // read address A, B and Write
     input RegWr, Clk;
     
-    reg [31:0] registers[31:0]; //32x32 register
+    reg [63:0] registers[31:0]; // 32x64 register
      
-    assign #2 BusA = (RA == 0) ? 0 : registers[RA]; //data from registers to specific bus
-    assign #2 BusB = (RB == 0) ? 0 : registers[RB];
+    assign #2 BusA = (RA == 0) ? 0 : registers[RA]; // data from registers to specific bus
+    assign #2 BusB = (RB == 0) ? 0 : registers[RB]; // 2 tic delay
      
-    always @ (negedge Clk) begin //based on negative clock edge
-        if(RegWr)
-            registers[RW] <= #3 BusW;
+    always @ (negedge Clk) begin // based on negative clock edge
+        if(RegWr && RW != 5'b11111) // prevent writes to reg 31
+            registers[RW] <= #3 BusW; // write with 3 tic delay
     end
-endmodule
+endmodule // end of module
