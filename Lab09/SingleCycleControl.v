7530344638107bd7624387fb0de45c3d683a7cd2
Josh M <81320874+Joshm196@users.noreply.github.com>
2025-04-11 16:03:17 -0500

    Update SingleCycleControl.v

--- Lab09/SingleCycleControl.v
+++ Lab09/SingleCycleControl.v
@@ -1,18 +1,18 @@
-`define OPCODE_ANDREG 11'b?0001010???
-`define OPCODE_ORRREG 11'b?0101010???
-`define OPCODE_ADDREG 11'b?0?01011???
-`define OPCODE_SUBREG 11'b?1?01011???
+`define OPCODE_ANDREG 11'b10001010000
+`define OPCODE_ORRREG 11'b10101010000
+`define OPCODE_ADDREG 11'b10001011000
+`define OPCODE_SUBREG 11'b11001011000
 
-`define OPCODE_ADDIMM 11'b?0?10001???
-`define OPCODE_SUBIMM 11'b?1?10001???
+`define OPCODE_ADDIMM 11'b1001000100x
+`define OPCODE_SUBIMM 11'b1101000100x
 
-`define OPCODE_MOVZ   11'b110100101??
+`define OPCODE_MOVZ   11'b11010010100
 
-`define OPCODE_B      11'b?00101?????
-`define OPCODE_CBZ    11'b?011010????
+`define OPCODE_B      11'b000101xxxxx
+`define OPCODE_CBZ    11'b10110100000
 
-`define OPCODE_LDUR   11'b??111000010
-`define OPCODE_STUR   11'b??111000000
+`define OPCODE_LDUR   11'b11111000010
+`define OPCODE_STUR   11'b11111000000
 
 module control(
 	       output reg 	reg2loc,
@@ -28,24 +28,159 @@ module control(
 	       input [10:0] 	opcode
 	       );
 
-   always @(*)
-     begin
-	casez (opcode)
+    always @(*)
+        begin
+            casez (opcode)
 
           /* Add cases here for each instruction your processor supports */
-
-          default:
+      
+			//R-Type: AND
+            OPCODE_ANDREG:
             begin
-               reg2loc       = 1'bx;
-               alusrc        = 1'bx;
-               mem2reg       = 1'bx;
-               regwrite      = 1'b0;
-               memread       = 1'b0;
-               memwrite      = 1'b0;
-               branch        = 1'b0;
-               uncond_branch = 1'b0;
-               aluop         = 4'bxxxx;
-               signop        = 2'bxx;
+                reg2loc       = 1'b0;
+                alusrc        = 1'b0;
+                mem2reg       = 1'b0;
+                regwrite      = 1'b1;
+                memread       = 1'b0;
+                memwrite      = 1'b0;
+                branch        = 1'b0;
+                uncond_branch = 1'b0;
+                aluop         = 4'b0000;
+                signop        = 2'bxx;
+            end
+
+			//R-Type: ORR
+            OPCODE_ORRREG:
+            begin
+                reg2loc       = 1'b0;
+                alusrc        = 1'b0;
+                mem2reg       = 1'b0;
+                regwrite      = 1'b1;
+                memread       = 1'b0;
+                memwrite      = 1'b0;
+                branch        = 1'b0;
+                uncond_branch = 1'b0;
+                aluop         = 4'b0001;
+                signop        = 2'bxx;
+            end
+
+			//R-Type: ADD
+            OPCODE_ADDREG:
+            begin
+                reg2loc       = 1'b0;
+                alusrc        = 1'b0;
+                mem2reg       = 1'b0;
+                regwrite      = 1'b1;
+                memread       = 1'b0;
+                memwrite      = 1'b0;
+                branch        = 1'b0;
+                uncond_branch = 1'b0;
+                aluop         = 4'b0010;
+                signop        = 2'bxx;
+            end
+
+			//R-Type: SUB
+            OPCODE_SUBREG:
+            begin
+                reg2loc       = 1'b0;
+                alusrc        = 1'b0;
+                mem2reg       = 1'b0;
+                regwrite      = 1'b1;
+                memread       = 1'b0;
+                memwrite      = 1'b0;
+                branch        = 1'b0;
+                uncond_branch = 1'b0;
+                aluop         = 4'b0110;
+                signop        = 2'bxx;
+            end
+
+			//I-Type: ADDI
+            OPCODE_ADDIMM:
+            begin
+                reg2loc       = 1'b0;
+                alusrc        = 1'b1;
+                mem2reg       = 1'b0;
+                regwrite      = 1'b1;
+                memread       = 1'b0;
+                memwrite      = 1'b0;
+                branch        = 1'b0;
+                uncond_branch = 1'b0;
+                aluop         = 4'b0010;
+                signop        = 2'b00;
+            end
+
+			//I-Type: SUBI
+            OPCODE_SUBIMM:
+            begin
+                reg2loc       = 1'b0;
+                alusrc        = 1'b1;
+                mem2reg       = 1'b0;
+                regwrite      = 1'b1;
+                memread       = 1'b0;
+                memwrite      = 1'b0;
+                branch        = 1'b0;
+                uncond_branch = 1'b0;
+                aluop         = 4'b0110;
+                signop        = 2'b00;
+            end
+
+			//
+            "OPCODE":
+            begin
+                reg2loc       = 1'b0;
+                alusrc        = 1'b0;
+                mem2reg       = 1'b0;
+                regwrite      = 1'b0;
+                memread       = 1'b0;
+                memwrite      = 1'b0;
+                branch        = 1'b0;
+                uncond_branch = 1'b0;
+                aluop         = 4'b0000;
+                signop        = 2'bxx;
+            end
+
+			//
+            "OPCODE":
+            begin
+                reg2loc       = 1'b0;
+                alusrc        = 1'b0;
+                mem2reg       = 1'b0;
+                regwrite      = 1'b0;
+                memread       = 1'b0;
+                memwrite      = 1'b0;
+                branch        = 1'b0;
+                uncond_branch = 1'b0;
+                aluop         = 4'b0000;
+                signop        = 2'bxx;
+            end
+
+			//
+            "OPCODE":
+            begin
+                reg2loc       = 1'b0;
+                alusrc        = 1'b0;
+                mem2reg       = 1'b0;
+                regwrite      = 1'b0;
+                memread       = 1'b0;
+                memwrite      = 1'b0;
+                branch        = 1'b0;
+                uncond_branch = 1'b0;
+                aluop         = 4'b0000;
+                signop        = 2'bxx;
+            end
+
+        default:
+            begin
+                reg2loc       = 1'bx;
+                alusrc        = 1'bx;
+                mem2reg       = 1'bx;
+                regwrite      = 1'b0;
+                memread       = 1'b0;
+                memwrite      = 1'b0;
+                branch        = 1'b0;
+                uncond_branch = 1'b0;
+                aluop         = 4'bxxxx;
+                signop        = 2'bxx;
             end
 	endcase
      end
