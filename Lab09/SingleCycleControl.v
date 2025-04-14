0c33439e6c5022856d8bc13079dcfbd71f834898
Sanjit Mukesh <105826663+sanjitmukesh@users.noreply.github.com>
2025-04-14 10:06:13 -0500

    Update SingleCycleControl_q1.v

--- Lab09/SingleCycleControl.v
+++ Lab09/SingleCycleControl.v
@@ -1,18 +1,18 @@
-`define OPCODE_ANDREG 11'b10001010000
-`define OPCODE_ORRREG 11'b10101010000
-`define OPCODE_ADDREG 11'b10001011000
-`define OPCODE_SUBREG 11'b11001011000
+`define OPCODE_ANDREG 11'b?0001010???
+`define OPCODE_ORRREG 11'b?0101010???
+`define OPCODE_ADDREG 11'b?0?01011???
+`define OPCODE_SUBREG 11'b?1?01011???
 
-`define OPCODE_ADDIMM 11'b1001000100x
-`define OPCODE_SUBIMM 11'b1101000100x
+`define OPCODE_ADDIMM 11'b?0?10001???
+`define OPCODE_SUBIMM 11'b?1?10001???
 
-`define OPCODE_MOVZ   11'b11010010100
+`define OPCODE_MOVZ   11'b110100101??
 
-`define OPCODE_B      11'b000101xxxxx
-`define OPCODE_CBZ    11'b10110100xxx
+`define OPCODE_B      11'b?00101?????
+`define OPCODE_CBZ    11'b?011010????
 
-`define OPCODE_LDUR   11'b11111000010
-`define OPCODE_STUR   11'b11111000000
+`define OPCODE_LDUR   11'b??111000010
+`define OPCODE_STUR   11'b??111000000
 
 module control(
 	       output reg 	reg2loc,
@@ -125,21 +125,6 @@ module control(
                 signop        = 2'b00;
             end
 
-			//MOVZ 
-            `OPCODE_MOVZ:
-            begin
-                reg2loc       = 1'bx;
-                alusrc        = 1'b1;
-                mem2reg       = 1'b0;
-                regwrite      = 1'b1;
-                memread       = 1'b0;
-                memwrite      = 1'b0;
-                branch        = 1'b0;
-                uncond_branch = 1'b0;
-                aluop         = 4'b1000;
-                signop        = 2'b00;
-            end
-
 			//B  
             `OPCODE_B:
             begin
@@ -149,7 +134,7 @@ module control(
                 regwrite      = 1'b0;
                 memread       = 1'b0;
                 memwrite      = 1'b0;
-                branch        = 1'b0;
+                branch        = 1'bx;
                 uncond_branch = 1'b1;
                 aluop         = 4'bxxxx;
                 signop        = 2'b10;
@@ -167,7 +152,7 @@ module control(
                 branch        = 1'b1;
                 uncond_branch = 1'b0;
                 aluop         = 4'b0111;
-                signop        = 2'b01;
+                signop        = 2'b11;
             end
 
 			//LDUR
@@ -182,7 +167,7 @@ module control(
                 branch        = 1'b0;
                 uncond_branch = 1'b0;
                 aluop         = 4'b0010;
-                signop        = 2'b11;
+                signop        = 2'b01;
             end
 
 			//STUR
@@ -190,14 +175,14 @@ module control(
             begin
                 reg2loc       = 1'b1;
                 alusrc        = 1'b1;
-                mem2reg       = 1'bx;
+                mem2reg       = 1'b0;
                 regwrite      = 1'b0;
                 memread       = 1'b0;
                 memwrite      = 1'b1;
                 branch        = 1'b0;
                 uncond_branch = 1'b0;
                 aluop         = 4'b0010;
-                signop        = 2'b11;
+                signop        = 2'b01;
             end
 
 
@@ -218,4 +203,3 @@ module control(
      end
 
 endmodule
-
